da83c4b0de2a8b71413ded0efe2941c0363fee72
Max Guclu <max.guclu@chasacademy.se>
2023-01-20 21:15:32 +0100

    Initial functionality Add: - User class - Mock users - function to create user - function to log in - test run

--- Program.cs
+++ Program.cs
@@ -4,9 +4,63 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! Test 2!");
+            Console.WriteLine("Welcome to Squid-bank");
+            //User newUser = Functions.CreateUser();
+            User mockUser1 = new User("Frank", 1111);
+            User mockUser2 = new User("Berit", 2222);
+            Console.WriteLine("Current users: " + mockUser1.name + " and " + mockUser2.name );
+            Functions.LogIn(mockUser1.name, mockUser1.pincode);
+        }
+    }
 
-            Console.WriteLine("Max Testar");
+
+    //Klass för användare
+    public class User
+    {
+        public string name { get; set; }
+        public int pincode { get; set; }
+
+        //Konstruktor
+        public User(string userName, int userPincode)
+        {
+            name = userName;
+            pincode = userPincode;
+        }
+    }
+
+    public class Functions
+    {
+        //Funktion för att skapa användare
+        public static User CreateUser()
+        {
+            Console.Write("Enter name of new user: ");
+            string inputName = Console.ReadLine();
+            Console.Write("Enter pincode for new user: ");
+            int inputPin = int.Parse(Console.ReadLine());
+            User newUser = new User(inputName, inputPin);
+            return newUser;
+        }
+
+        //Funktion för inlogg
+        //Funktion för att säkerställa att rätt typ av input ges int/string**
+
+        public static bool LogIn(string logInName, int logInPin) //
+        {
+            Console.WriteLine("Enter username: ");
+            string inputUserName = Console.ReadLine();
+            Console.WriteLine("Enter pincode: ");
+            int inputPinCode = int.Parse(Console.ReadLine());
+
+            if (inputUserName == logInName && inputPinCode == logInPin)
+            {
+                Console.WriteLine("Välkommen");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Fel inloggningsuppgifter");
+                return false;
+            }
         }
     }
 }
