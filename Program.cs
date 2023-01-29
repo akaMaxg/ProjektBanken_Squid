1b67fa4226b4ff5101b803b5f0ecb8b582995b0f
Mostafa Shubber <m_shubber@hotmail.com>
2023-01-29 13:13:53 +0100

    Account

--- Program.cs
+++ Program.cs
@@ -9,25 +9,40 @@ namespace ProjektBankenSquid2
         static void Main(string[] args)
         {
             //.....Change the pathfile in the database clase to your local db folder......
-            Console.WriteLine("Welcome to Squid-bank");
-            List <User> users = database.LoadBankUsers();
 
-            foreach (User user in users)
+
+            Console.WriteLine("Enter your id");
+            int userid = int.Parse(Console.ReadLine());
+
+            List<Account> accounts = database.UserAccount(userid);
+
+            foreach (var item in accounts)
             {
-                Console.WriteLine($"Hello {user.first_name} your last name is {user.last_name}");
+                Console.WriteLine($"{item.name} {item.interest_rate}, {item.balance}");
             }
+            
 
-            Console.Write("Enter name: ");
-            string firstName = Console.ReadLine();
-            Console.Write("Enter pincode: ");
-            string pinCode = Console.ReadLine();
-            users = database.CheckLogin(firstName, pinCode);
-            foreach (User user in users)
-            {
-                Console.WriteLine($"Log in successfull, welcome {user.first_name} {user.last_name}");
-            }
+            //Console.WriteLine("Welcome to Squid-bank");
+            //List <User> users = database.LoadBankUsers();
 
-            database.Transfer(users[0].id, 4, 1000, 1000);
+            //foreach (User user in users)
+            //{
+            //    Console.WriteLine($"Hello {user.first_name} your last name is {user.last_name}");
+            //}
+
+            //Console.Write("Enter name: ");
+            //string firstName = Console.ReadLine();
+            //Console.Write("Enter pincode: ");
+            //string pinCode = Console.ReadLine();
+            //users = database.CheckLogin(firstName, pinCode);
+            //foreach (User user in users)
+            //{
+            //    Console.WriteLine($"Log in successfull, welcome {user.first_name} {user.last_name}");
+            //}
+
+           
+
+            //database.Transfer(users[0].id, 4, 1000, 1000);
 
             //User newUser = new User("Frank", "Sinatra", "1111");
             //database.SaveBankUser(newUser);
