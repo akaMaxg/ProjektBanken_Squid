22913294645b151349eadc6c71e042cc79872ed9
Max Guclu <max.guclu@chasacademy.se>
2023-01-29 11:40:19 +0100

    Database update Connected to monsters db Added login function Updated User class Started transfer function Updated gitignore ----- Removed dbsquid Removed old db code Removed old functions

--- Functions.cs
+++ Functions.cs
@@ -3,60 +3,40 @@
     public class Functions
     {
         //Funktion för att skapa användare
-        public static User CreateUser()
-        {
-            Console.Write("Enter name of new user: ");
-            string inputName = Console.ReadLine();
-            Console.Write("Enter pincode for new user: ");
-            int inputPin = int.Parse(Console.ReadLine());
-            User newUser = new User(inputName, inputPin);
-            return newUser;
-        }
+
 
         //Funktion för inlogg
         //Funktion för att säkerställa att rätt typ av input ges int/string**
 
-        public static bool LogIn() // Vill vi hämta in alla användare som en lista? Vill vi injecta användarens input i SQL query och hämta därifrån? Vill vi göra en variant? Vill vi ha kodsnipp så en användare inte kan skriva egen SQL
-        {
-            bool check = false;
-            Console.WriteLine("Enter username: ");
-            string inputUserName = Console.ReadLine();
-            List<string> users = database.GetUsers();
-            foreach (var item in users)
-            {
-                Console.WriteLine(item);
-                if (inputUserName == item) // Add second check for pincode if username = item
-                {
-                    Console.WriteLine("Välkommen");
-                    check = true;
-                }
-            }
-            if (check == false) // Add second check for pincode if username = item
-            {
-                Console.WriteLine("Fel inloggningsuppgifter");
-            }
-            return false;
-        }
         public static void MockLogIn() //
         {
             //User newUser = Functions.CreateUser();
-            User mockUser1 = new User("Frank", 1111);
-            User mockUser2 = new User("Berit", 2222);
-            Console.WriteLine("Current users: " + mockUser1.name + " and " + mockUser2.name);
-            Console.WriteLine("Enter username: ");
-            string inputUserName = Console.ReadLine();
-            Console.WriteLine("Enter pincode: ");
-            int inputPinCode = int.Parse(Console.ReadLine());
+            //User mockUser1 = new User("Frank", "1111");
+            //User mockUser2 = new User("Berit", "2222");
+            //Console.WriteLine("Current users: " + mockUser1.first_name + " and " + mockUser2.first_name);
+            //Console.WriteLine("Enter username: ");
+            //string inputUserName = Console.ReadLine();
+            //Console.WriteLine("Enter pincode: ");
+            //int inputPinCode = int.Parse(Console.ReadLine());
 
-            if (inputUserName == mockUser1.name && inputPinCode == mockUser1.pincode || inputUserName == mockUser2.name && inputPinCode == mockUser2.pincode)
-            {
-                Console.WriteLine("Välkommen!");
-            }
-            else
-            {
-                Console.WriteLine("Fel");
-            }
+            //if (inputUserName == mockUser1.first_name && inputPinCode == mockUser1.pin_code || inputUserName == mockUser2.first_name && inputPinCode == mockUser2.pin_code)
+            //{
+            //    Console.WriteLine("Välkommen!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Fel");
+            //}
         }
+        //public static User CreateUser()
+        //{
+        //    Console.Write("Enter name of new user: ");
+        //    string inputName = Console.ReadLine();
+        //    Console.Write("Enter pincode for new user: ");
+        //    string inputPin = Console.ReadLine();
+        //    User newUser = new User(inputName, inputPin);
+        //    return newUser;
+        //}
     }
 }
 
--- User.cs
+++ User.cs
@@ -1,16 +1,28 @@
-﻿namespace ProjektBankenSquid2
+﻿using Npgsql.Replication.PgOutput.Messages;
+
+namespace ProjektBankenSquid2
 {
-    //Klass för användare
+    //Klass för mock-användare
     public class User
     {
-        public string name { get; set; }
-        public int pincode { get; set; }
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string pin_code { get; set; }
+    }
+    public class MockUser
+    {
+        public int mockid { get; set; }
+        public string mockfirst_name { get; set; }
+        public string mocklast_name { get; set; }
+        public string mockpin_code { get; set; }
 
         //Konstruktor
-        public User(string userName, int userPincode)
+        public MockUser(string firstName, string lastName, string pinCode)
         {
-            name = userName;
-            pincode = userPincode;
+            mockfirst_name = firstName;
+            mocklast_name = lastName;
+            mockpin_code = pinCode;
         }
     }
 }
